bf91100b3522556663d704d30ba9c84318f65fb4
Manob-chandra <135313849+Manob-chandra@users.noreply.github.com>
2026-03-27 16:29:29 +0600

    Load content dynamically from HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("My portfolio site is connected successfully.");
+fetch("assets/experience_academic_bio/experience_academic_bio.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("content-area").innerHTML = data;
+  })
+  .catch(error => {
+    document.getElementById("content-area").innerHTML = "<p>Content load hoyni.</p>";
+    console.error("Error:", error);
+  });
